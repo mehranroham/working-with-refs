0e92a2cd961f5197ab8ae4bc1476aa9704cd2a87
Mehran Roham <mehranroham@gmail.com>
2024-04-10 19:03:47 +0330

    using states

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [name, setName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event) => {
+    setSubmitted(false);
+    setName(event.target.value);
+  };
+
+  const handleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {submitted ? name : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type='text' value={name} onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
